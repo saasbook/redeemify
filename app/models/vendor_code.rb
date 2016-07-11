95307c6bdb1a105a067513af6833cd72d53bd53f
Matt Lindsey <matthew.r.lindsey@gmail.com>
2016-07-11 12:40:20 -0400

    add missing vendor_code model fields

--- app/models/vendor_code.rb
+++ app/models/vendor_code.rb
@@ -1,5 +1,5 @@
 class VendorCode < ActiveRecord::Base
 	belongs_to :vendor
 	belongs_to :user
-    attr_accessor :code, :name , :vendor , :user_id, :vendor_id , :user_name, :email
+    attr_accessor :code, :name , :vendor , :user_id, :vendor_id , :user_name, :email, :instruction, :help, :expiration
 end
