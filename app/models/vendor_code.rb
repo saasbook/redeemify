707d1fcbd9e82e46a866cc519023d620eab4d58a
Matt Lindsey <matthew.r.lindsey@gmail.com>
2016-07-13 08:03:37 -0400

    add fields to VendorCode model

--- app/models/vendor_code.rb
+++ app/models/vendor_code.rb
@@ -2,5 +2,6 @@ class VendorCode < ActiveRecord::Base
 	belongs_to :vendor
 	belongs_to :user
 	# TODO: USE STRONG PARAMETERS
-	# attr_accessible :code, :name , :vendor , :user_id, :vendor_id , :user_name, :email, :instruction, :help, :expiration
+	# attr_accessible :code, :name , :vendor , :user_id, :vendor_id , :user_name, :email,
+	attr_accessor :instruction, :help, :expiration
 end
