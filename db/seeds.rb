cf72ccf03ed1b0bf5d15182cd2611b94a653b17e
Matt Lindsey <matthew.r.lindsey@gmail.com>
2016-10-30 18:10:28 -0400

    Fix seeds.rb to add some dev data

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,17 +3,22 @@
 
 raise "I refuse to destroy the production database!" if Rails.env.production?
 
+# SuperUser
 AdminUser.create!(:email => 'admin@example.com', :password => 'password', :password_confirmation => 'password')
 
 case Rails.env
  when "development"
-   User.create!(provider: "Amazon", uid: 111, name: "Amazon.com")
- 
-   Provider.create!(name: "Amazon.com")
-   ProviderCode.create!(code: "zyxwv")
- 
-   Vendor.create!(name: "VendorOne", uid: 111, provider: "github", description: "Github (1 mth free micro)",
-     website: "google.com", helpLink: "google.com/help", cashValue: 25)
-   VendorCode.create(code: 'xP78vvjk3V', vendor_id: 111, user_id: 123, name: "GitHub Micro", user_name: "UserOne")
- end
+   Provider.create!(name: "Amazon")
 
+   RedeemifyCode.create!(code: "zyxwv", provider_id: 1)
+   RedeemifyCode.create!(code: "yyyyy", provider_id: 1)
+   RedeemifyCode.create!(code: "zzzzz", provider_id: 1)
+
+   # Add myself as a Vendor User to admininster the Amazon $10 vendor codes
+   # Other developers feel free to create a PR to add yourself and some vendor codes
+   User.create!(provider: "Amazon", uid: 123, name: "matthew.r.lindsey@gmail.com")
+   Vendor.create!(name: "www.amazon.com", uid: 123, provider: "Amazon", description: "$10 AWS Credit",
+                  website: "amazon.com", helpLink: "amazon.com/help", cashValue: "$10")
+   VendorCode.create(code: 'xP78vvjk3V', vendor_id: 1)
+
+ end
