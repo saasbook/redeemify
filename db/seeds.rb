71346d05d9f222664a90a8b3de55115ab84c82b3
Matt Lindsey <matthew.r.lindsey@gmail.com>
2016-10-31 09:59:57 -0400

    Update seeds.rb to add creating a test Provider

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,13 +7,22 @@ AdminUser.create!(:email => 'admin@example.com', :password => 'password', :passw
 
 case Rails.env
  when "development"
-   User.create!(provider: "Amazon", uid: 111, name: "Amazon.com")
- 
-   Provider.create!(name: "Amazon.com")
-   ProviderCode.create!(code: "zyxwv")
- 
-   Vendor.create!(name: "VendorOne", uid: 111, provider: "github", description: "Github (1 mth free micro)",
-     website: "google.com", helpLink: "google.com/help", cashValue: 25)
-   VendorCode.create(code: 'xP78vvjk3V', vendor_id: 111, user_id: 123, name: "GitHub Micro", user_name: "UserOne")
- end
+   Provider.create!(name: "Amazon")
 
+   RedeemifyCode.create!(code: "zyxwv", provider_id: 1)
+   RedeemifyCode.create!(code: "yyyyy", provider_id: 1)
+   RedeemifyCode.create!(code: "zzzzz", provider_id: 1)
+
+   # Add a Vendor to admininster the Amazon $10 vendor codes
+   Vendor.create!(name: "www.amazon.com", provider: "amazon", description: "$10 AWS Credit",
+                  website: "amazon.com", helpLink: "amazon.com/help", cashValue: "$10")
+   VendorCode.create(code: 'xP78vvjk3K', vendor_id: 1)
+   VendorCode.create(code: 'yF35ovem3V', vendor_id: 1)
+   VendorCode.create(code: 'zL92pcje3M', vendor_id: 1)
+
+   # Add myself as a "Google" Provider User to test Provider code uploads
+   # Other developers feel free to create a PR to add yourself
+   # Note that you must also modify config/application.yml to contain the GOOGLE_KEY and GOOGLE_SECRET for your
+   # Google+ account as described at https://developers.google.com/+/web/api/rest/oauth
+   Provider.create!(name: "Google",provider: "google_oauth2", email: "matthew.r.lindsey@gmail.com")
+ end
