670e6d19fc6cf0c0628a29ade3ce09ca836da8c7
Matt Lindsey <matthew.r.lindsey@gmail.com>
2016-10-30 22:04:54 -0400

    Add myself as a Provider in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,13 +14,16 @@ case Rails.env
    RedeemifyCode.create!(code: "yyyyy", provider_id: 1)
    RedeemifyCode.create!(code: "zzzzz", provider_id: 1)
 
-   # Add myself as a Vendor User to admininster the Amazon $10 vendor codes
-   # Other developers feel free to create a PR to add yourself and some vendor codes
-   User.create!(provider: "Amazon", uid: 123, name: "matthew.r.lindsey@gmail.com")
-   Vendor.create!(name: "www.amazon.com", uid: 123, provider: "Amazon", description: "$10 AWS Credit",
+   # Add a Vendor to admininster the Amazon $10 vendor codes
+   Vendor.create!(name: "www.amazon.com", provider: "amazon", description: "$10 AWS Credit",
                   website: "amazon.com", helpLink: "amazon.com/help", cashValue: "$10")
    VendorCode.create(code: 'xP78vvjk3K', vendor_id: 1)
    VendorCode.create(code: 'yF35ovem3V', vendor_id: 1)
    VendorCode.create(code: 'zL92pcje3M', vendor_id: 1)
 
+   # Add myself as a "Google" Provider User to test Provider code uploads
+   # Other developers feel free to create a PR to add yourself
+   # Note that you must also modify config/application.yml to contain the GOOGLE_KEY and GOOGLE_SECRET for your
+   # Google+ account as described at https://developers.google.com/+/web/api/rest/oauth
+   Provider.create!(name: "Google",provider: "google_oauth2", email: "matthew.r.lindsey@gmail.com")
  end
