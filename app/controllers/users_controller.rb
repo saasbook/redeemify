af9823b68ce4809c558e1dd011f157ad6d078a97
Matt Lindsey <matthew.r.lindsey@gmail.com>
2016-11-20 05:57:41 -0500

    remove unneeded code from users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,2 @@
 class UsersController < ApplicationController
-
-  # This method is here temporarily so that the cucumber api_get.feature passes
-  # both when running cucumber and on the CI environment.
-  # Otherwise we would need somthing like 'prax' running.
-  def index
-    render json: {
-         code: '12345'
-    }.to_json
-  end
 end
