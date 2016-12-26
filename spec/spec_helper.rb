0f66c51d08d0c0e063c8318fed2bde7a743a0b3b
alexander_kh <khlipun@gmail.com>
2016-12-26 12:38:22 +0000

    Change RSpec configuration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -145,7 +145,7 @@ RSpec.configure do |config|
   config.include Request::JsonHelpers, :type => :controller
   config.include Request::HeadersHelpers, :type => :controller
   
-  config.before(:each, type: :controller) do
+  config.before(:each, controller: Api::V1::UsersController) do
     include_default_accept_headers
   end
 
