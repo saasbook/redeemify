b0905ff181c1b8418586034a470c701046f6d89a
alexander_kh <khlipun@gmail.com>
2016-12-26 16:00:08 +0000

    Make changes in vendors_controller.rb and vendor_spec.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,4 +35,5 @@ case Rails.env
    # account as in http://docs.aws.amazon.com/general/latest/gr/aws-sec-cred-types.html#access-keys-and-secret-access-keys
    Vendor.create!(name: "Amazon", provider: "amazon_oath2", email: "matthew.r.lindsey@gmail.com",  description: "AWS Testing",
                   website: "matthewrlindsey.org", helpLink: "matthewrlindsey.org/help", cashValue: "$8")
+   Vendor.create!(name: "Alexander", provider: "google_oauth2", email: "khlipun@gmail.com")
 end
