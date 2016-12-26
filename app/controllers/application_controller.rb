528f7913704da0d2c99be11d97b3a7e288dad4e5
alexander_kh <khlipun@gmail.com>
2016-12-26 12:48:24 +0000

    Change protect_from_forgery option

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,7 @@
 class ApplicationController < ActionController::Base
   # protect_from_forgery
 
-  # protect_from_forgery with: :exception
-  protect_from_forgery with: :null_session
+  protect_from_forgery with: :exception
   helper_method :current_user, :current_provider
 
 private
