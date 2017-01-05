78670e9a843b43af869a188fc00745bf72ccdee4
Uzzsen <uzzsen@gmail.com>
2017-01-05 16:51:41 +0300

    Refactored sessions_controller#delete_account + cucumber scenario update + specs (#66)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -83,20 +83,11 @@ class SessionsController < ApplicationController
   end
 
   def delete_account
-#    current_user = User.find(session[:user_id])
     if current_user != nil
-      current_user.update_attributes(:name => "anonymous", :email => "anonymous", :provider => "anonymous")
-      redeemifyCode = RedeemifyCode.where(:user_id => current_user.id).first
-      redeemifyCode.update_attributes(:user_name =>"anonymous", :email => "anonymous")
-      
-      vendors = Vendor.all
-      vendors.each do |vendor|
-        vendorCode = vendor.vendorCodes.where(:user_id => current_user.id).first
-        if vendorCode != nil
-          vendorCode.update_attributes(:user_name => "anonymous", :email => "anonymous")
-          
-        end
-      end
+      current_user.anonymize!
+      RedeemifyCode.anonymize! current_user
+      VendorCode.anonymize_all! current_user
+
       session[:user_id] = nil
       redirect_to root_url, :flash => { :notice => "Your account has been deleted." }
     end
